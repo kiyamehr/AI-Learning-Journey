997d12b9971974c49afd20365617eb11b543d8cf
kiyamehr <kiamehrmoradii@gmail.com>
2026-07-30 19:17:06 +0330

    Added helper function create_tf_dataset

--- Practice Labs/Unsupervised Learning/Recommender Systems/02-Practice/utils.py
+++ Practice Labs/Unsupervised Learning/Recommender Systems/02-Practice/utils.py
@@ -1,4 +1,5 @@
 from sklearn.preprocessing import LabelEncoder
+import tensorflow as tf
 
 def column_encoder(dataset, *cols):
     encoders = {}
@@ -9,3 +10,14 @@ def column_encoder(dataset, *cols):
         encoders[col] = encoder
         
     return dataset, encoders
+
+def create_tf_dataset(data, target, *cols):
+    features = {
+        col: data[col].values for col in cols
+    }
+    
+    labels = data[target].values
+    
+    tf_ds = tf.data.Dataset.from_tensor_slices((features, labels))
+    
+    return tf_ds
