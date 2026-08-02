518edbce899f5b6adec904011ba80fc65631ea84
kiyamehr <kiamehrmoradii@gmail.com>
2026-08-02 21:24:56 +0330

    Added The Recommender Model class code here

---
+++ Practice Labs/Unsupervised Learning/Recommender Systems/Content Based Filtering/Project_1_Movielens/Models/recommender_model_code.py
@@ -0,0 +1,74 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
+
+@tf.keras.utils.register_keras_serializable()
+class MovieRecommender(tf.keras.Model):
+    
+    def __init__(self, num_users, num_movie_features=19, embedding_dim=32, l2_lambda=0.001, **kwargs):
+        super().__init__(**kwargs)
+        self.num_users = num_users
+        self.num_movie_features = num_movie_features
+        self.embedding_dim = embedding_dim
+        self.l2_lambda = l2_lambda
+        self.l2_reg = tf.keras.regularizers.l2(l2_lambda)
+
+        # -------------------------
+        # User Network
+        # -------------------------
+
+        self.user_NN = tf.keras.models.Sequential([
+            
+            tf.keras.Input(shape=(1,), dtype=tf.float32),
+            
+            tf.keras.layers.Embedding(
+                input_dim=num_users+1,
+                output_dim=embedding_dim,
+                embeddings_regularizer=self.l2_reg
+            ),
+            tf.keras.layers.Flatten()
+        ], name='user_model')
+
+        # -------------------------
+        # Movie Network
+        # -------------------------
+        self.movie_NN = tf.keras.models.Sequential([
+            
+            tf.keras.Input(shape=(num_movie_features,), dtype=tf.float32),
+            
+            Dense(256, activation='relu', name='movieL1', kernel_regularizer=self.l2_reg),
+            Dense(128, activation='relu', name='movieL2', kernel_regularizer=self.l2_reg),
+            Dense(embedding_dim, activation='linear', name='movieL3', kernel_regularizer=self.l2_reg),
+        ], name='movie_model')
+        
+        # -------------------------
+        # Dot Product
+        # -------------------------
+        self.dot_product = tf.keras.layers.Dot(axes=1, name='dot_product')
+
+    def call(self, inputs):
+        
+        user_input, movie_input = inputs
+            
+        # Actual output of the model
+        vu = self.user_NN(user_input)
+
+        # Actual output: 32-dimensional movie representation
+        vm = self.movie_NN(movie_input)
+
+        # Calc Dot Product of the output from the two models
+        output = self.dot_product([vu, vm])
+
+        return output    
+    
+    def get_config(self):
+
+        config = super().get_config()
+
+        config.update({
+            "num_users": self.num_users,
+            "num_movie_features": self.num_movie_features,
+            "embedding_dim": self.embedding_dim,
+            "l2_lambda": self.l2_lambda
+        })
+
+        return config
