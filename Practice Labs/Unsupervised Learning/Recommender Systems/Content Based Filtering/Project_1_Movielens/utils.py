03490e7ee6608a196ca5deb04627c63df637bd43
kiyamehr <kiamehrmoradii@gmail.com>
2026-08-02 11:29:48 +0330

    Added prepare_recommender_data Function

--- Practice Labs/Unsupervised Learning/Recommender Systems/Content Based Filtering/Project_1_Movielens/utils.py
+++ Practice Labs/Unsupervised Learning/Recommender Systems/Content Based Filtering/Project_1_Movielens/utils.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 def multi_hot_encode(items, column, separator="|"):
     
     encoded_rows = []
@@ -15,3 +17,24 @@ def multi_hot_encode(items, column, separator="|"):
         encoded_rows.append(encoded_array)
 
     return encoded_rows
+
+def prepare_recommender_data(train_data, cv_data):
+    
+    # Training Data
+    X_user_train = train_data['userId'].values
+    y_train = train_data['rating'].values
+    
+    X_movie_train = np.stack(train_data['genres_encoded'].values)
+    
+    train_movie_ids = train_data['movieId'].values
+
+    #  --------------------------------------------------------------
+
+    # Cross Validation Data
+    X_user_cv = cv_data['userId'].values
+    X_movie_cv = np.stack(cv_data['genres_encoded'].values)
+    
+    y_cv = cv_data['rating'].values
+    cv_movie_ids = cv_data['movieId'].values
+    
+    return X_user_train, y_train, X_movie_train, train_movie_ids, X_user_cv, X_movie_cv, y_cv, cv_movie_ids
