8b01ac8f4400b38d4900579a1cceafb25fbc99b7
kiyamehr <kiamehrmoradii@gmail.com>
2026-08-13 18:01:35 +0330

    Added apply pca function

--- Projects/Kaggle Challenges/01-Housing Prices/utils.py
+++ Projects/Kaggle Challenges/01-Housing Prices/utils.py
@@ -1,4 +1,5 @@
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.decomposition import PCA
 import pandas as pd
 
 def one_hot_encode(df, *columns):
@@ -37,3 +38,26 @@ def one_hot_encode(df, *columns):
     result = pd.concat([result, encoded_df], axis=1)
 
     return result
+
+def apply_pca(df, n_components=2):
+
+    """
+    Reduce the dimensionality of a dataset using PCA.
+
+    Parameters
+    ----------
+    df : pandas.DataFrame
+        The input features.
+    n_components : int, default=2
+        Number of principal components to keep.
+
+    Returns
+    -------
+    tuple
+        PCA-transformed data and the fitted PCA object.
+    """
+
+    pca = PCA(n_components=n_components, copy=True, svd_solver='auto')
+    transformed_data = pca.fit_transform(df)
+    
+    return transformed_data, pca
